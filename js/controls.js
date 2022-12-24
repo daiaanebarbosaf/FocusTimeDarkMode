ac3ff4832552609341a010b0ccd6a8619c3ec14a
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2022-12-24 11:11:03 -0300

    Create controls.js

---
+++ js/controls.js
@@ -0,0 +1,46 @@
+export default function Controls({
+  buttonPlay,
+  buttonPause,
+  buttonStop,
+  buttonSet
+}) {
+
+  function play() {
+    buttonPlay.classList.add('hide')
+    buttonPause.classList.remove('hide')
+    buttonStop.classList.add('hide')
+    buttonSet.classList.remove('hide')
+  }
+
+  function pause() {
+    buttonPause.classList.add('hide')
+    buttonPlay.classList.remove('hide')
+    buttonSet.classList.add('hide')
+    buttonStop.classList.remove('hide')
+  }
+
+  function reset() {
+    buttonPlay.classList.remove('hide')
+    buttonPause.classList.add('hide')
+    buttonSet.classList.remove('hide')
+    buttonStop.classList.add('hide')
+  }
+
+  function getMinutes() {
+    let newMinutes = prompt('Quantos minutos?')
+
+    if (!newMinutes) {
+      return false
+    }
+  
+    return newMinutes
+  }
+
+  return {
+    reset, 
+    play, 
+    pause,
+    getMinutes
+  }
+}
+
