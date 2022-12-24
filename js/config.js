d9a5629250ef3c15ef5e870ce3ca2ecf38000fe8
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2022-12-24 11:10:58 -0300

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,21 @@
+const buttonPlay = document.querySelector('.play')
+const buttonPause = document.querySelector('.pause')
+const buttonStop = document.querySelector('.stop')
+const buttonSet = document.querySelector('.set')
+
+const buttonvolumeHigh = document.querySelector('.volume-high')
+const buttonvolumeLow = document.querySelector('.volume-low')
+
+const minutesDisplay = document.querySelector('.minutes')
+const secondsDisplay = document.querySelector('.seconds')
+
+export {
+  buttonPlay,
+  buttonPause,
+  buttonStop,
+  buttonSet,
+  buttonvolumeHigh,
+  buttonvolumeLow,
+  minutesDisplay,
+  secondsDisplay
+}
